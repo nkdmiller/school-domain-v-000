a807a80ca1ad11cc260d6254de69a340e7de44fc
Nate Miller <natekmiller96@gmail.com>
2018-06-26 22:31:53 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
-# code here!
+class School
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+    roster[grade] = []
+    roster[grade] << name
+  end
+
+  def roster
+    roster
+  end
+
+  def grade(grade)
+    roster[grade]
+  end
+
+end
+    
+    
+    
